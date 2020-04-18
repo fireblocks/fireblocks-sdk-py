f96d6efc35a10aa1b90998fa7d475b0ff34880c5
Michael Poluy <michaelp@fireblocks.io>
2020-04-18 23:22:39 +0300

    addition on hidden accunts

--- fireblocks_sdk/sdk.py
+++ fireblocks_sdk/sdk.py
@@ -243,14 +243,30 @@ class FireblocksSDK(object):
 
         return self._post_request(f"/v1/transactions/{txid}/cancel")
 
-    def create_vault_account(self, name):
+    def create_vault_account(self, name, hiddenOnUI = False):
         """Creates a new vault account.
 
         Args:
             name (str): A name for the new vault account
         """
 
-        return self._post_request("/v1/vault/accounts", {"name": name})
+        return self._post_request("/v1/vault/accounts", {"name": name, "hiddenOnUI": hiddenOnUI})
+    
+    def hide_vault_account(self, vault_account_id):
+        """Hides the vault account from being visible in the web console
+
+        Args:
+            vault_account_id (str): The vault account Id
+        """
+        return self._post_request(f"/v1/vault/accounts/{vault_account_id}/hide")
+
+    def unhide_vault_account(self, vault_account_id):
+        """Returns the vault account to being visible in the web console
+
+        Args:
+            vault_account_id (str): The vault account Id
+        """
+        return self._post_request(f"/v1/vault/accounts/{vault_account_id}/unhide")
 
     def create_vault_asset(self, vault_account_id, asset_id):
         """Creates a new asset within an existing vault account
