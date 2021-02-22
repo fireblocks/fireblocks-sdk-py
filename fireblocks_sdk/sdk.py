e91c7288169461bff7315b2456a01073d2605db1
Michael Poluy <michaelp@fireblocks.io>
2021-02-23 01:13:26 +0200

    filters rename

--- fireblocks_sdk/sdk.py
+++ fireblocks_sdk/sdk.py
@@ -855,7 +855,7 @@ class FireblocksSDK(object):
 
         return self._put_request(url, body)
     
-    def get_vault_assets_balance(self, name_prefix=None, name_suffix=None):
+    def get_vault_assets_balance(self, account_name_prefix=None, account_name_suffix=None):
         """Gets vault assets accumulated balance
         
          Args:
@@ -866,11 +866,11 @@ class FireblocksSDK(object):
 
         params = {}
         
-        if name_prefix:
-            params['namePrefix'] = name_prefix
+        if account_name_prefix:
+            params['accountNamePrefix'] = account_name_prefix
         
-        if name_suffix:
-            params['nameSuffix'] = name_suffix
+        if account_name_suffix:
+            params['accountNameSuffix'] = account_name_suffix
             
         if params:
             url = url + "?" + urllib.parse.urlencode(params)
