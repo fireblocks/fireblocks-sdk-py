dcf8f1e9c01cb2b566e8c370f37385f08ef5efb5
Tomer Vilensky <tomerv@fireblocks.com>
2021-05-01 23:28:17 +0300

    Freeze/Unfreeze transaction

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
   name = 'fireblocks_sdk',
   packages = ['fireblocks_sdk'],
-  version = '1.5.15',
+  version = '1.5.16',
   license='MIT',
   description = 'Fireblocks python SDK',
   url = 'https://github.com/fireblocks/fireblocks-sdk-py',
-  download_url = 'https://github.com/fireblocks/fireblocks-sdk-py/archive/v1.5.15.tar.gz',
+  download_url = 'https://github.com/fireblocks/fireblocks-sdk-py/archive/v1.5.16.tar.gz',
   keywords = ['Fireblocks', 'SDK'],
   install_requires=[
           'PyJWT==1.7.1',
@@ -16,7 +16,7 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Software Development',    
+    'Topic :: Software Development',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
   ],
