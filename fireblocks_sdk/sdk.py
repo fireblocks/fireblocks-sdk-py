b7637eceda3aa126c71184ec48dd5586079575d8
amirlevy <amir@fireblocks.com>
2022-01-03 17:37:30 +0200

    COR-10778 add idempotency

--- fireblocks_sdk/sdk.py
+++ fireblocks_sdk/sdk.py
@@ -101,14 +101,14 @@ class FireblocksSDK(object):
 
         return self._get_request(f"/v1/vault/accounts/{vault_account_id}/{asset_id}")
 
-    def refresh_vault_asset_balance(self, vault_account_id, asset_id):
+    def refresh_vault_asset_balance(self, vault_account_id, asset_id, idempotency_key=None):
         """Gets a single vault account asset after forcing refresh from the blockchain
         Args:
             vault_account_id (string): The id of the requested account
             asset_id (string): The symbol of the requested asset (e.g BTC, ETH)
         """
 
-        return self._post_request(f"/v1/vault/accounts/{vault_account_id}/{asset_id}/balance")
+        return self._post_request(f"/v1/vault/accounts/{vault_account_id}/{asset_id}/balance", {}, idempotency_key)
 
     def get_deposit_addresses(self, vault_account_id, asset_id):
         """Gets deposit addresses for an asset in a vault account
