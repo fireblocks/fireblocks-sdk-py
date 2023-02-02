aba7e3732976b18baacbb5374ed155f1e6fae271
vsiskin <vsiskin@fireblocks.com>
2023-02-02 09:35:29 +0100

    small fix

--- fireblocks_sdk/sdk.py
+++ fireblocks_sdk/sdk.py
@@ -119,7 +119,7 @@ class FireblocksSDK(object):
         return self._get_request(url, query_params=params)
 
     def get_owned_nfts(self, blockchain_descriptor: str, vault_account_ids: List[str] = None, ids: List[str] = None,
-                       collectionIds: List[str] = None, page_cursor: str = '', page_size: int = 100, sort: List[GetOwnedNftsSortValues] = None, order: OrderValues = None):
+                       collection_ids: List[str] = None, page_cursor: str = '', page_size: int = 100, sort: List[GetOwnedNftsSortValues] = None, order: OrderValues = None):
         """
 
         """
