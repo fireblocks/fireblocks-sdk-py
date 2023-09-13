793b272c559bfbd52b8cee207a2ff875ee0b38a7
Yanir Levi <ylevi@fireblocks.com>
2023-09-13 10:32:25 -0500

    Removed old class from init

--- fireblocks_sdk/__init__.py
+++ fireblocks_sdk/__init__.py
@@ -1,3 +1,3 @@
-from fireblocks_sdk.sdk import FireblocksSDK, FireblocksNCW
+from fireblocks_sdk.sdk import FireblocksSDK
 from fireblocks_sdk.sdk_token_provider import SdkTokenProvider
 from fireblocks_sdk.api_types import *
