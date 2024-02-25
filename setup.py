85f87625ff452b4aaa0adc01f63d7d3f6238689e
YoavBZ <yoav.bz4@gmail.com>
2024-02-25 05:59:52 +0200

    Update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setup(
           'requests>=2.22.0',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
     'Topic :: Software Development',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
   ],
 )
