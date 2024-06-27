8aae1f9d623ebe3285dd1152628666b336d513c3
bpayne@fireblocks.com <bpayne@fireblocks.com>
2024-06-27 12:45:44 +0300

    added a rescan_transactions

--- fireblocks_sdk/sdk.py
+++ fireblocks_sdk/sdk.py
@@ -40,6 +40,7 @@ from .api_types import (
     SpamTokenOwnershipValues,
     TokenOwnershipSpamUpdatePayload,
     TokenOwnershipSpamUpdatePayload,
+    RescanTx,
 )
 from .tokenization_api_types import \
     CreateTokenRequest, \
@@ -2089,6 +2090,17 @@ class FireblocksSDK:
 
         return self._get_request(url)
 
+    def rescan_transactions(self, rescan_txs):
+        """initiate rescan for given transactions
+        Args:
+            rescan_txs: (Array of RescanTx): the transaction asset_id and Hash for rescan
+                Each RescanTx should have the following keys:
+                - 'assetId': string
+                - 'txHash': String
+        """
+        path = f"/v1/rescan/transactions"
+        body = {"transactions": rescan_txs}
+        return self._post_request(path, body)
     def get_paginated_addresses(self, vault_account_id, asset_id, limit=500, before=None, after=None):
         """Gets a paginated response of the addresses for a given vault account and asset
         Args:
